845f5c7d9f0d9d807c3f02e126170e161385fd57
nikithanalubala-ctrl <nikithanalubala@gmail.com>
2026-07-31 10:25:50 +0530

    added env

--- models/bronze/bronze_sales.sql
+++ models/bronze/bronze_sales.sql
@@ -1,3 +1,19 @@
-{{config(materialized='view')}}
 
-SELECT * FROM {{source('source','fact_sales')}}
+
+{{ config(
+    materialized='incremental',
+    unique_key='sales_id'
+) }}
+
+select *
+from {{ source('source','fact_sales') }}
+
+{% if is_incremental() %}
+
+where sales_id >
+(
+    select max(sales_id)
+    from {{ this }}
+)
+
+{% endif %}
